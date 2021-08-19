ed8ecf1c8dc8e749b5c454de4c2f0d277ad4583e
Collin F. Lynch <cflynch@lin-res11.csc.ncsu.edu>
2021-08-19 00:10:51 -0400

    Adding server management code.

---
+++ servermanagement/RunLearningObserver.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env/bash
+# ===============================
+# RunLearningObserver.sh
+# Collin F. Lynch
+#
+# This bash script provides a simple wrapper to run the 
+# learning observer service and pipe the data to a logfile
+# over time this should be integrated into the systemd 
+# service process.  This uses static variables to specify
+# the location of the virtualenv and the command and 
+# specifies the location for the running logfile. 
+
+# System Variables
+# --------------------------------------
+VIRTUALENV_PYTHON="/usr/local/share/Projects/WritingObserver/VirtualENVs/learning_observer/bin/python3.8"
+LEARNING_OBSERVER_LOC="/usr/local/share/Projects/WritingObserver/Repo-Fork/writing_observer/learning_observer"
+LOGFILE_DEST="/usr/local/share/Projects/WritingObserver/Repo-Fork/writing_observer/learning_observer/learning_observer/logs"
+
+# Make the logfile name
+# ---------------------------------------
+LOG_DATE=$(date "+%m-%d-%Y--%H-%M-%S")
+LOG_NAME="$LOGFILE_DEST/learning_observer_service_$LOG_DATE.log"
+echo $LOG_NAME;
+
+ 
+
+
+
+
+# learning_observer
