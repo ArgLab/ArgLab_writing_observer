76b6bf28b321ba4a9137ebbb3f9f74842317d665
JohnDamilola <johndamilola03@gmail.com>
2023-09-28 15:51:22 -0400

    refactor blacklisting to use a yaml config file

--- learning_observer/learning_observer/auth/blacklisting_settings.py
+++ learning_observer/learning_observer/auth/blacklisting_settings.py
@@ -1,25 +1,33 @@
 """
-This file contains constants for blacklisting/authentication rule types, their responses, priorities and patterns.
+This file contains constants, patterns, and functions related to blacklisting/authentication rules and responses.
 
 RULES_RESPONSES
 A dictionary containing responses for different rule types, including:
 - type (str)
-- message (str)
+- msg (str)
 - status_code (int)
-
-RULES_PATTERNS
-A dictionary containing patterns to match against for different rule types, specifying:
-- field (str)
-- patterns (arrays of regex patterns)
-to apply for rule evaluation.
 
 RULE_TYPES_BY_PRIORITIES
 A list defining the priority order of rule types for sorting.
+
+load_patterns(file_path='blacklisting_patterns.yaml')
+A function that loads blacklisting patterns from a YAML file and returns them as a dictionary.
+
+authenticate_payload(payload)
+A function that evaluates a payload against a set of rules and determines the authentication response.
 """
+
+
+import re
+import yaml
+from learning_observer.log_event import debug_log
 
 ALLOW = "allow"
 DENY = "deny"
 DENY_FOR_TWO_DAYS = "deny_for_two_days"
+
+# Priority order of rule types for sorting
+RULE_TYPES_BY_PRIORITIES = [DENY, DENY_FOR_TWO_DAYS]
 
 # Responses for different rule types
 RULES_RESPONSES = {
@@ -40,25 +48,51 @@ RULES_RESPONSES = {
     }
 }
 
-# Patterns to match against for different rule types
-RULES_PATTERNS = {
-    DENY: [
-        {
-            "field": "email",
-            "patterns": ["^.*@ncsu.edu"]
-        },
-        {
-            "field": "google_id",
-            "patterns": ["1234"]
-        }
-    ],
-    DENY_FOR_TWO_DAYS: [
-        {
-            "field": "email",
-            "patterns": ["^.*@ncsu.edu"]
-        }
-    ]
-}
 
-# Priority order of rule types for sorting
-RULE_TYPES_BY_PRIORITIES = [DENY, DENY_FOR_TWO_DAYS]
+def load_patterns(file_path='blacklisting_patterns.yaml'):
+    try:
+        with open(file_path, 'r') as file:
+            rules_patterns = yaml.safe_load(file)
+        return rules_patterns
+    except FileNotFoundError:
+        debug_log(f"No blacklisting patterns file added: '{file_path}' not found.")
+        return {}
+
+
+def authenticate_payload(payload):
+    '''
+    Evaluate a payload against a set of rules and determine the authentication response.
+
+    This function iterates through a list of rules for various fields in the payload.
+    For each field, it checks if the value matches any of the specified patterns.
+    If a match is found, the associated rule type is added to a list of failed rule types.
+    The failed rule types are then sorted by priority, and the highest priority failed rule
+    determines the authentication response.
+
+    If no rules fail, the function returns the 'allow' response.
+
+    Args:
+        payload (dict): The payload containing data to be authenticated.
+
+    Returns:
+        str: The authentication response based on the payload and rule evaluation.
+    '''
+    RULES_PATTERNS = load_patterns()
+    failed_rule_types = []  # A list to store rule types that the payload fails to comply with
+    for rule_type, rules in RULES_PATTERNS.items():
+        for rule in rules:
+            field = rule["field"]  # Get the field to be looked up in the payload
+            patterns = rule["patterns"]  # Get the patterns to match against for the payload value of the field
+            value = payload.get(field)  # Get the value of the field from the payload
+            if value:
+                for pattern in patterns:
+                    # If there is a pattern match, add the rule type to the failed list
+                    if re.match(pattern, value):
+                        failed_rule_types.append(rule_type)
+
+    # Sort the failed rule types based on their priority order
+    sorted_failed_rule_types = sorted(failed_rule_types, key=RULE_TYPES_BY_PRIORITIES.index)
+    # Determine the response key based on the highest priority failed rule, or 'allow' if no rule failed
+    response_key = sorted_failed_rule_types[0] if sorted_failed_rule_types else ALLOW
+    # Return the appropriate response based on the response key
+    return RULES_RESPONSES[response_key]
